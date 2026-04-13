f1382d691e81cf54d016be187ff9554e2a5b2cd8
worthbeer <67826062+worthbeer@users.noreply.github.com>
2026-04-13 12:28:29 -0400

    initial commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello from Acquisitions')
+})
+
+export default app;
